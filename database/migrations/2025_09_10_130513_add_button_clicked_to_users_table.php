471b986326758fc3c96cc360747fb5bbadecdc0e
francesco ercolani <francesco.ercolani3@studio.unibo.it>
2025-09-11 11:46:08 +0200

    Add migration, new column added to users table

---
+++ database/migrations/2025_09_10_130513_add_button_clicked_to_users_table.php
@@ -0,0 +1,28 @@
+ration create_flights_table<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedInteger('button_clicked')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('button_clicked');
+        });
+    }
+};
